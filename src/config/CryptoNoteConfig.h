c35e4878f598e38268beb219e598b6fbabf67dce
Woolong Coin <50668946+Woolong-coin@users.noreply.github.com>
2019-08-12 10:57:33 -0700

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -169,7 +169,12 @@ static_assert(0 < UPGRADE_VOTING_THRESHOLD && UPGRADE_VOTING_THRESHOLD <= 100, "
 static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 
 /* Block heights we are going to have hard forks at */
-const uint64_t FORK_HEIGHTS[] = {};
+const uint64_t FORK_HEIGHTS[] = {
+  500000,  // 0
+  1000000, // 1
+  1500000, // 2
+  2000000,
+};
 
 /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
 const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX                 = 0;
